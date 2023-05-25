21781351b91eadffe6e58eb227d5e96346481b66
stanis <111862414+Project-230822@users.noreply.github.com>
2023-05-25 13:20:19 +0300

    Дополнил delivery.calculation

---
+++ local/components/custom/delivery.calculation/.description.php
@@ -0,0 +1,19 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => "Заготовка",
+	"DESCRIPTION" => "",
+	"ICON" => "",
+	"SORT" => 10,
+	"CACHE_PATH" => "Y",
+	"PATH" => array(
+		"ID" => "Custom", // for example "my_project"
+		"NAME" => "Кастомные"
+		/*"CHILD" => array(
+			"ID" => "", // for example "my_project:services"
+			"NAME" => "",  // for example "Services"
+		),*/
+	),
+	"COMPLEX" => "N",
+);
