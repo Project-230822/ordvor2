8f5f345b0def942c77a11e81db8ce865a37fd6cd
stanis <111862414+Project-230822@users.noreply.github.com>
2023-05-25 13:24:09 +0300

    Дополнил delivery.calculation

---
+++ local/components/custom/delivery.calculation/lang/ru/.parameters.php
@@ -0,0 +1,9 @@
+<?
+$MESS['C_IBLOCK_TYPE'] = "Тип инфоблока";
+$MESS['C_IBLOCK_ID'] = "ID инфоблока";
+$MESS['C_SECTION_1_LEVEL'] = "Список разделов 1 уровня";
+$MESS['C_SECTION_2_LEVEL'] = "Список разделов 2 уровня";
+$MESS['C_SECTION_3_LEVEL'] = "Список разделов 3 уровня";
+$MESS['C_LIST_OF_ELEMENTS'] = "Список элементов";
+$MESS['C_PROPERTY_LIST'] = "Список свойств";
+$MESS['C_WRITE_YOUR_TITLE'] = "Написать заголовок для страницы";
