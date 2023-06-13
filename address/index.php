8c0b0b8418600e3323533f90c3aea24adb477846
Just-guy <andrey.belyy.95@mail.ru>
2023-06-13 09:28:01 +0300

    Перенос страницы «Наши магазины»

--- address/index.php
+++ address/index.php
@@ -1,30 +1,37 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Наши магазины");
 
 $obCity = OrdvorCity::getInstance();
 
-$APPLICATION->SetTitle("Магазины Оружейный двор Мир приключений Моя рыбалочка в " . $obCity->getDeclination("IN_CITY"));
-
 $description = "Магазины Оружейный двор Мир приключений Моя рыбалочка в " . $obCity->getDeclination("IN_CITY");
-$APPLICATION->SetPageProperty("description", $description);
-
-?>
-
-<h1 class="page-title">Контакты</h1>
-
-<div class="content">
-	<?php
-		$APPLICATION->IncludeComponent(
-		"bitrix:main.include",
-		"",
-		Array(
-			"AREA_FILE_RECURSIVE" => "Y",
-			"AREA_FILE_SHOW" => "file",
-			"AREA_FILE_SUFFIX" => "inc",
-			"EDIT_TEMPLATE" => "",
-			"PATH" => SITE_DIR."include/address.php"
-		)
-	);?>	
+$APPLICATION->SetPageProperty("description", $description); ?>
+<? $APPLICATION->GetTitle(false) ?>
+<h1 class="page-title"><? $APPLICATION->ShowTitle(false); ?></h1>
+<div class="our-stores-page">
+	<? $APPLICATION->IncludeComponent(
+		"custom:our.stores",
+		".default",
+		array(
+			"CACHE_TIME" => "3600",
+			"CACHE_TYPE" => "A",
+			"IBLOCK_ID" => "35",
+			"IBLOCK_TYPE" => "content",
+			"WRITE_YOUR_TITLE" => "",
+			"COMPONENT_TEMPLATE" => ".default"
+		),
+		false
+	); ?>
 </div>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? $APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	array(
+		"AREA_FILE_RECURSIVE" => "Y",
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => SITE_DIR . "include/address.php"
+	)
+); ?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
